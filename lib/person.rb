658ac1034ea5efef464fa15fa673395eaf22c502
Robert <r_lee94@yahoo.com>
2019-04-03 15:31:15 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,18 @@
+class Person
+  def job
+    @job
+  end
+  
+  def job=(new_person)
+    @job = new_person
+  end
+
+def name=(new_name)
+  @name = new_name
+end
+
+def name
+  @name
+end
+ 
+ end 
